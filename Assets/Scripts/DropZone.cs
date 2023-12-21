08fca8dc26aa1975fe5396220b8899560d0c9041
Logan <loganhobson31@gmail.com>
2023-12-21 04:13:02 -0600

    Drag and Drop Map works

---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DragDrop dragItem = dropped.GetComponent<DragDrop>();
+            dragItem.parentAfterDrag = transform;
+        }
+        else
+        {
+            //Combine fleets
+        }
+    }
+
+  
+}
